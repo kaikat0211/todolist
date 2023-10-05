38b679d78f88f56a19b93e4844d9743559369831
kaikat0211 <kaifat7887@gmail.com>
2023-10-05 12:05:04 +0800

    fix apos

--- components/Register/Email.js
+++ components/Register/Email.js
@@ -15,7 +15,7 @@ export default function Email({formData,setFormData}) {
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" onChange={emailSubmit} required/>
             <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
+            We&apos;ll never share your email with anyone else.
             </Form.Text>
         </Form.Group>
     </>
